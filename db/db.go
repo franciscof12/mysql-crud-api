aa6df17fae51118b994f134468616eca438abcad
Francisco Ferreira <franciscoferrefc@gmail.com>
2023-10-19 15:01:22 +0200

    tengo dos neuronas

--- db/db.go
+++ db/db.go
@@ -6,7 +6,7 @@ import (
 )
 
 func Connect() (*gorm.DB, error) {
-	dsn := "dbuser:dbpassword@tcp(127.0.0.1:3306)/gofiberddbb?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "mydbuser:mydbpassword@tcp(db:3306)/gofiberddbb?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
